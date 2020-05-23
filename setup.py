e05fa4091b5ae9d8c0f71c2984d722c6884a70ae
Abhishek Singh <a.s.2013.hts@gmail.com>
2020-05-23 10:18:06 +0530

    Update details

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 # Encoding: UTF-8
 # Line Endings: LF(Unix)
-# Programming Language: Python v3.7
+# Programming Language: Python 3
 # Standard: PEP 8
 
 '''
 Author: Abhishek Singh
 email: a.s.2013.hts@gmail.com
-Copyright: (C) 2020 Abhishek Singh
+Source: https://github.com/singh2505/bkliz | see README for more details
 License: GNU General Public License v3 (GPLv3) | see LICENSE for more details
-Source Code: https://github.com/singh2505/bkliz | see README for more details
-
 Contribution: Wanna contribute to bkliz library? | see CONTRIBUTING for more details
+
+Copyright (C) 2020 Abhishek Singh
 '''
 
 import setuptools
@@ -21,7 +21,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bkliz",
-    version="1.0.4",
+    version="1.0.5",
     author="Abhishek Singh",
     author_email="a.s.2013.hts@gmail.com",
     description="A Secure and Powerful Open Source Python Library for Encryption/Decryption.",
@@ -30,9 +30,19 @@ setuptools.setup(
     url="https://github.com/singh2505/bkliz",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    keyword='python-library pypi-package encryption-decryption utf-8 gplv3',
 )
