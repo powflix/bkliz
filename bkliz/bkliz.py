8de4c9b72c48185fbe3b3fa63c15bb5cbe54ce0f
Abhishek Singh <51530258+realsingh25@users.noreply.github.com>
2020-06-17 14:16:39 +0530

    Update UTF-8 Character Range

--- bkliz/bkliz.py
+++ bkliz/bkliz.py
@@ -3,23 +3,27 @@
 # Programming Language: Python 3
 # Syntax Standard: PEP 8
 
-"""Copyright (C) 2020 Powflix Inc., and its affiliates.
+
+"""A Secure and Powerful Open Source Python Library for Encryption/Decryption.
 
 Source: https://github.com/powflix/bkliz | see README for more details.
 License: GNU General Public License v3 (GPLv3) | see LICENSE for more details.
 Contribution: Wanna contribute to bkliz? | see CONTRIBUTING for more details.
+
+Copyright (C) 2020 Powflix Inc., and its affiliates.
 """
+
 
 import random
 
 
 def encode(self):
-    """Encryption function of bkliz library."""
+    """Encryption Method of bkliz library."""
     storedkey = []
     storedmsg = ''
 
     for character in self:
-        key = random.randint(0, 255)
+        key = random.randint(33, 126)       # UTF - 8 Character Range
 
         storedmsg += chr(ord(character) + key)
         storedkey.append(key)
@@ -27,18 +31,18 @@ def encode(self):
     for character in storedkey:
         storedmsg += chr(character + len(self))
 
-    del(storedkey)
+    del(storedkey)      # Deleting the key generated for encryption
 
     return storedmsg
 
 
 def decode(self):
-    """Decryption function of bkliz library."""
+    """Decryption Method of bkliz library."""
     storedkey = []
     storedmsg = ''
     counter = 0
 
-    midpoint = int(len(self) / 2)
+    midpoint = int(len(self)/2)
 
     for character in self[midpoint:]:
         storedkey.append(ord(character) - midpoint)
@@ -47,7 +51,8 @@ def decode(self):
         storedmsg += chr(ord(character) - storedkey[counter])
         counter += 1
 
-    del(storedkey)
-    del(midpoint)
+    del(storedkey)      # Deleting the key generated for decryption
+    del(midpoint)       # Deleting temporary variable
+    del(counter)        # Deleting temporary variable
 
     return storedmsg
