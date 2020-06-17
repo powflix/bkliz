ca71fad9934ce07c3ddf49d1909de2ed3bb0191e
Abhishek Singh <51530258+realsingh25@users.noreply.github.com>
2020-06-17 14:17:43 +0530

    Project urls added

--- setup.py
+++ setup.py
@@ -1,34 +1,38 @@
-#!/usr/bin/env python3
-
 # Encoding: UTF-8
 # Line Endings: LF(Unix)
 # Programming Language: Python 3
 # Syntax Standard: PEP 8
 
-"""Copyright (C) 2020 Powflix Inc., and its affiliates.
+
+"""A Secure and Powerful Open Source Python Library for Encryption/Decryption.
 
 Source: https://github.com/powflix/bkliz | see README for more details.
 License: GNU General Public License v3 (GPLv3) | see LICENSE for more details.
 Contribution: Wanna contribute to bkliz? | see CONTRIBUTING for more details.
+
+Copyright (C) 2020 Powflix Inc., and its affiliates.
 """
+
 
 import setuptools
 
-with open("README.md", "r") as fh:
+
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="bkliz",
-    version="1.0.8",
+    version="1.0.9",
     author="Powflix Inc.",
     author_email="Powflix@outlook.com",
     description="""A Secure and Powerful Open Source Python Library for
                     Encryption/Decryption.""",
 
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     url="https://github.com/powflix/bkliz",
     packages=setuptools.find_packages(),
+
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -43,6 +47,15 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
+
     python_requires=">=3.6",
     keywords="python-library pypi-package encryption-decryption utf-8 gplv3",
+
+    project_urls={
+        "Bug Reports": "https://github.com/powflix/bkliz/issues",
+        "Say Thanks!": "https://saythanks.io/to/powflix%40outlook.com",
+        "Source": "https://github.com/powflix/bkliz/",
+        "Documentation": """https://github.com/powflix/bkliz
+                            /blob/master/README.rst"""
+    },
 )
